9d70fef218b710607bac33f9646379b532beec81
Egogorka <bike034@mail.ru>
2021-09-18 11:08:23 +0300

    polygon

--- src/Polygon/Polygon.h
+++ src/Polygon/Polygon.h
@@ -5,9 +5,17 @@
 #ifndef POLYGONS_MIPT_SEM3_POLYGON_H
 #define POLYGONS_MIPT_SEM3_POLYGON_H
 
+#include "Dot/Dot.h"
 
 class Polygon {
+protected:
+    int n;
+    Dot* dots;
 
+public:
+    Polygon(Dots* in, int size);
+
+    float perimeter();
 };
 
 
